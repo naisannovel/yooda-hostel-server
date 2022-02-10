bdd2efbe639e88ba9f78bd7fc8f66a15b195c28e
naisannovel <naisannovel@gmail.com>
2022-02-10 16:33:29 +0600

    update

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
+const DB = process.env.MONGODB_SERVER.replace('<password>',process.env.DB_PASSWORD);
 
-mongoose.connect('mongodb://localhost:27017/users')
+mongoose.connect(DB)
 .then(()=>console.log('mongodb connected successfully'))
 .catch(()=>console.log('mongodb connection failed'))
 
